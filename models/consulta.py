8899d61e9831d2a5361b96104645f746f226d539
Giancarlo <giancarlo.paredes.urquieta@gmail.com>
2019-03-06 10:51:52 -0500

    impresion desde formulario

--- models/consulta.py
+++ models/consulta.py
@@ -8,7 +8,7 @@ class Consulta(models.Model):
     _name = 'tickets.consulta'
     _description = "Consulta"
 
-    # log = Log()
+    log = logger.Log()
 
     modelo = fields.Char(
         string="Modelo",
@@ -29,13 +29,22 @@ class Consulta(models.Model):
         string='Consultar Artículos'
     )
 
+    printer_id = fields.Many2one(
+        comodel_name='printing.printer', string='Printer', required=True,
+        help='Printer used to print the labels.')
+
+    label_id = fields.Many2one(
+        comodel_name='printing.label.zpl2', string='Label', required=True,
+        # domain=lambda self: [
+        #     ('model_id.model', '=', self.env.context.get('active_model'))],
+        help='Label to print.')
+
     @api.multi
     @api.onchange('consultar_articulos')
     def onchange_consultar_articulos(self):
         if self.modelo and self.almacen:
             articulos = sapdata.get_articulos(self.modelo, self.almacen)
-            log = logger.Log()
-            log.logger.info(articulos)
+            self.log.logger.info(articulos)
             valores = []
             for item in articulos:
                 valores += [
@@ -50,6 +59,17 @@ class Consulta(models.Model):
                     })
                 ]
             self.articulos = valores
+
+    @api.multi
+    def imprimir_ticket(self):
+        self.env['printing.label.zpl2'].browse(self.label_id.id).print_label(
+            self.env['printing.printer'].browse(self.printer_id.id),
+            self.env['tickets.consulta'].browse(self.id)
+        )
+        self.log.logger.info(self.env['printing.label.zpl2'].browse(self.label_id))
+        self.log.logger.info(self.env['printing.printer'].browse(self.printer_id))
+        self.log.logger.info(self.env['tickets.consulta'].browse(self.id))
+        return
 
 
 class Data(models.TransientModel):
